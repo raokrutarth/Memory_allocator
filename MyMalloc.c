b12491d08de9b497ed920567bb628efcdd0b2329
Krutarth <raok@purdue.edu>
2016-06-16 02:48:15 -0400

    Autocommit (Makefile)

--- MyMalloc.c
+++ MyMalloc.c
@@ -315,7 +315,7 @@ void freeObject( void * ptr ) /*################################################
 	}
 	//toFree = the coalasced block header
 	//update next and prev pointers in freeList and in new free (coalesced block) after finding right position
-	for (temph = _freeList->_next; !(toFree > temph && toFree < temph->_next); temph = temph->_next)
+	for (temph = _freeList->_next; temph != _freeList; temph = temph->_next)
 		if (temph >= temph->_next && (toFree > temph || toFree < temph->_next))
 			break; //temph at block which should be after toFree	
 	toFree->_prev = temph;//toFree->_prev = temph->_prev;
