96d2177ffa5e443fc288beedb1b16945acb1eac9
Krutarth <raok@purdue.edu>
2016-06-16 03:37:46 -0400

    Autocommit (Makefile)

--- MyMalloc.c
+++ MyMalloc.c
@@ -321,7 +321,7 @@ void freeObject( void * ptr ) /*################################################
     if (temph >= temph->_next && (toFree > temph || toFree < temph->_next))
       break; //temph at block which should be after toFree	*/
   temph = _freeList->_next;
-  while ( temph != _freeList && temph > toFree)
+  while ( temph->_next != _freeList && temph->_next > toFree)
     temph = temph->_next;
 
 	toFree->_next = temph->_next;
