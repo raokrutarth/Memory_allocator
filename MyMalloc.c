49206879e3e09c64bacbe461fe38fb915d6aea93
Krutarth <raok@purdue.edu>
2016-06-17 14:17:00 -0400

    Autocommit (Makefile)

--- MyMalloc.c
+++ MyMalloc.c
@@ -285,7 +285,7 @@ void insertFree_R(struct ObjectHeader * toFree, struct ObjectHeader * right)
 void insertFree_LR(struct ObjectHeader * toFree, struct ObjectHeader * right)
 {
   struct ObjectHeader *temph = getPlace(toFree);  
-  toFree->_next = temph->_next->_next;
+  toFree->_next = right->_next;
   temph->_next->_prev = toFree;
 }
 void freeObject( void * ptr ) /*###########################################################*/
